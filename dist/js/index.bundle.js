46dc4b8b7356d90aebe96d76f2b349db46b62d03
Shau <fa6248122@gmail.com>
2023-12-08 21:17:18 +0300

    Git

--- dist/js/index.bundle.js
+++ dist/js/index.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:174ab423893c134f05eca5bd507f7217b24f9d3adec5f31f0d97d533a5e9ed87
-size 2402
+oid sha256:7d3e346e21818016995a1cfa16bad8be42ea5bb245bd0440734b2bb8688bac90
+size 2584
